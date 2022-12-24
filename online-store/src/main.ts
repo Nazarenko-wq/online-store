600f088d08615e445c618db18cf62f52c6d143ba
Max Khotsin <slimgos22@gmail.com>
2022-12-24 17:14:31 +0300

    feat: add gallery page without filter section

--- online-store/src/main.ts
+++
@@ -1,5 +0,0 @@
-// import logo from '../public/logo.svg';
-
-// let logoImg = (<HTMLTemplateElement>document.querySelector('.logo'));
-// let logoTemplate = document.createElement('img').classList.add('logo-image') as ;
-// logoTemplate.src = logo;
