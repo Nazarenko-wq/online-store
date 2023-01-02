c570af2fbdb71be5d5d25037c7ac67f51a548450
Max Khotsin <slimgos22@gmail.com>
2023-01-02 21:53:58 +0300

    feat: add part1 of filter

--- online-store/src/assets/ts/shop.ts
+++ online-store/src/assets/ts/shop.ts
@@ -1,24 +1,74 @@
+import { renderImages } from "../../main";
+import database, { IData } from "./database";
+
+let acceptFiltersBtn = document.querySelector('.submit-filter');
 let categoryFilter = (<Element>document.querySelector('.filter-category'));
 let priceFilter = (<Element>document.querySelector('.filter-price'));
-window.isFilterOpened = false;
+let filterCloseBtns = (<NodeList>document.querySelectorAll('.button'));
+let resetBtn = document.querySelector('.reset-btn');
+let priceMinVal = (<HTMLInputElement>document.querySelector('.input-min'));
+let priceMaxVal = (<HTMLInputElement>document.querySelector('.input-max'));
+
+let copyOfDatabase = [...database];
 
 let arrFilters = [categoryFilter, priceFilter];
 arrFilters.forEach(filter => {
     filter?.addEventListener('click', () => {
         filter.lastElementChild?.classList.add('is-clicked');
-        window.isFilterOpened = true;
     })
 });
 
-// window.addEventListener('click', (e) => {
-//     if (window.isFilterOpened === true && e.target?.closest(arrFilters)) {
+filterCloseBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        setTimeout(() => {
+            btn.parentElement?.classList.remove('is-clicked');
+        }, 0);
+    })
+});
 
-//     }
-// })
+function filteredRender(e: Event) {
+    e.preventDefault();
+    if (document.querySelectorAll('[name="category"]:checked').length ||
+    priceMinVal.value !== '' ||
+    priceMaxVal.value !== ''
+    ) {
+        let wrapper = document.querySelector('.gallery-wrapper');
+        wrapper!.innerHTML = '';
+        
+        let filterItems = Array.from((<NodeListOf<HTMLInputElement>>document.querySelectorAll('[name="category"]:checked'))).map(checkbox => checkbox.value);
+        let filteredArr = [...database].filter(item => {
+            let pass = false;
+            if (filterItems.length) {
+                if (filterItems.includes(item.category)) {
+                    pass = true;
+                } else {
+                    pass = false;
+                }
+            }
+            let priceNatural = parseInt(item.price.replace('$', ''));
+            if (priceMinVal.value) {
+                if (parseInt(priceMinVal.value) > priceNatural) {
+                    pass = false;
+                } else {
+                    pass = true;
+                }
+            }
+            if (priceMaxVal.value) {
+                if (parseInt(priceMaxVal.value) > priceNatural) {
+                    pass = true;
+                } else {
+                    pass = false;
+                }
+            }
+            return pass;
+        });
+        renderImages(filteredArr);    
+    }
+}
 
-// let filters = document.querySelectorAll('.options-block')
-// .forEach(filter => {
-//     filter.parentElement?.addEventListener('click', () => {
-//         filter.classList.toggle('is-clicked');
-//     })
-// })
+acceptFiltersBtn?.addEventListener('click', filteredRender);
+resetBtn?.addEventListener('click', () => {
+    let wrapper = document.querySelector('.gallery-wrapper');
+    wrapper!.innerHTML = '';
+    renderImages(copyOfDatabase);
+})
--- online-store/src/main.ts
+++ online-store/src/main.ts
@@ -2,24 +2,27 @@ import database, { IData } from './assets/ts/database';
 import './assets/ts/burger'
 import {Card} from './assets/ts/card_create';
 
-let cardsToPage: number;
-
-let cardsWrapper = (<Element>document.querySelector('.gallery-wrapper'));
-
-if (cardsWrapper) {
-    if (cardsWrapper.classList.contains('gallery-page')) {
-        cardsToPage = 22;
-    } else {
-        cardsToPage = 3;
-    }    
-}
 let databaseCopy: Array<IData> = [...database];
 
-let randoms: Array<number> = new Array(databaseCopy.length).fill(0).map((_, i) => i).sort((_, __) => Math.random() - 0.5);
+export function renderImages(cardsDatabase: IData[]) {
+    let cardsToPage: number;
+    let cardsWrapper = (<Element>document.querySelector('.gallery-wrapper'));
 
-for (let i = 0; i < cardsToPage!; i++) {
-    let random: number = randoms.pop() as number;
-    let databaseElem = database[random];
-    let item = new Card(databaseElem.id, databaseElem.category, databaseElem.name, databaseElem.description, databaseElem.price, databaseElem.photo);
-    cardsWrapper.appendChild(item.init());
+    if (cardsWrapper) {
+        if (cardsWrapper.classList.contains('gallery-page')) {
+            cardsToPage = cardsDatabase.length;
+        } else {
+            cardsToPage = 3;
+        }    
+    }
+    let randoms: Array<number> = new Array(cardsDatabase.length).fill(0).map((_, i) => i).sort((_, __) => Math.random() - 0.5);
+
+    for (let i = 0; i < cardsToPage!; i++) {
+        let random: number = randoms.pop() as number;
+        let databaseElem = cardsDatabase[random];
+        let item = new Card(databaseElem.id, databaseElem.category, databaseElem.name, databaseElem.description, databaseElem.price, databaseElem.photo);
+        cardsWrapper.appendChild(item.init());
+    }
 }
+
+renderImages(databaseCopy);
