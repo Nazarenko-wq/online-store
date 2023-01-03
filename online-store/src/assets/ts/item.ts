53f432e43571ed8cb56be86c8688752e285adf5a
Mihail Nazarenko <82104821+Nazarenko-wq@users.noreply.github.com>
2023-01-03 15:14:06 +0300

    feat: add dinamic data to item page

--- online-store/src/assets/ts/item.ts
+++ online-store/src/assets/ts/item.ts
@@ -1,38 +1,108 @@
 import database from './database'
 
-let items = document.querySelectorAll('.gallery-item');
+(function(){
+    // Get all gallery items
+let items = document.querySelectorAll('[data-id]');
 
-// let currentPath: string = '';
-
-
+// Get current item ID & go to item page 
 items.forEach(elem => {
     elem.addEventListener('click', function(e) {
         
-        let currentImg = <HTMLImageElement>elem.children[0].firstElementChild;
+        localStorage.setItem('currentItemId', `${elem.attributes[1].nodeValue}`);
         let target = e.target as HTMLDivElement;
-        let currentPath = currentImg.src;
         
         if(target.classList.contains('item-image')) {
             window.location.href = './item.html';
         } 
-
-        function setItemDiscription (str: string) {
-            let itemImg = <HTMLImageElement>document.querySelector('.itemImg');
-            
-            database.forEach(elem => {
-                if(elem.photo[1].slice(-20) === str.slice(-20) ) {
-                    itemImg.src = elem.photo[1];
-                    // console.log('same');
-                    console.log(elem.photo[1]);
-                }
-               
-            })  
-
-            // console.log(str.slice(-20));
-        }
-        
-        setItemDiscription(currentPath)
     })
 })
+})();
 
-// console.log(currentPath)
+// Get item data
+(function(){
+    const id = localStorage.getItem('currentItemId');
+    let num = Number(id);
+    // console.log(num);
+
+    let itemImg = document.querySelector('.itemImg') as HTMLImageElement;
+    let itemPrice = document.querySelector('.cust') as HTMLParagraphElement;
+    let itemName = document.querySelector('.current-item') as HTMLSpanElement;
+    let itemKind = document.querySelector('.kinds') as HTMLSpanElement;
+    
+    database.forEach(elem => {
+        if(elem.id === num) {
+            itemImg.src = elem.photo[0];
+            itemPrice.textContent = elem.price;
+            itemName.textContent = elem.name;
+            itemKind.textContent = elem.category;
+        }
+    })
+})();
+
+// Choose the size
+(function(){
+    let parent = document.querySelector('.sizes') as HTMLDivElement;
+
+    parent.addEventListener('click', (e) => {
+        let target = e.target as HTMLDivElement;
+
+        let sizes = document.querySelectorAll('.size')
+        sizes.forEach(item => {
+            item.classList.remove('active-size');
+        })
+
+        target.classList.add('active-size');
+
+        // Push size to local storage
+        sizes.forEach(elem => {
+            if(elem.classList.contains('active-size')) {
+                localStorage.setItem('currentSize', `${elem.attributes[1].nodeValue}`)
+            }
+        })
+
+        // console.log(localStorage.getItem('currentSize'));
+    })
+})();
+
+// Choose the color
+(function(){
+    let parent = document.querySelector('.colors') as HTMLDivElement;
+
+    parent.addEventListener('click', (e) => {
+        let target = e.target as HTMLDivElement;
+
+        let colors = document.querySelectorAll('.color');
+        colors.forEach(item => {
+            item.classList.remove('active-color');
+        })
+
+        target.classList.add('active-color');
+
+        // Push size to local storage
+        colors.forEach(elem => {
+            if(elem.classList.contains('active-color')) {
+                localStorage.setItem('currentColor', `${elem.attributes[1].nodeValue}`)
+            }
+        })
+
+        // console.log(localStorage.getItem('currentColor'));
+    })
+})();
+
+
+// Push to lockal storage quantity goods
+(function(){
+    let input = document.querySelector('.quantity') as HTMLInputElement;
+    localStorage.setItem('currentQuantity', `${input.value}`);
+    
+    input.addEventListener('change', () => {
+        
+        localStorage.setItem('currentQuantity', `${input.value}`);
+        
+        // console.log(localStorage.getItem('currentQuantity'));
+    })
+})();
+
+// add btn listener
+let btn = document.querySelector('.add-btn') as HTMLButtonElement;
+btn.addEventListener('click', () => {window.location.href = './cart.html'});
