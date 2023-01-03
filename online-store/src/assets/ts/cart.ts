4350d7d7a50b71c03e2721f94ccf1cf140d76262
Mihail Nazarenko <82104821+Nazarenko-wq@users.noreply.github.com>
2023-01-03 20:01:58 +0300

    feat: add condition for item & cart page

--- online-store/src/assets/ts/cart.ts
+++ online-store/src/assets/ts/cart.ts
@@ -1,20 +1,29 @@
-// import database from './database';
+import database from './database';
 
-// let cartItemImg = document.querySelector('.cartImg') as HTMLImageElement;
-// let cartItemName = document.querySelector('.discription') as HTMLDivElement;
-// let cartItemPrice = document.querySelector('.price') as HTMLDivElement;
-// let cartItemQuantity = document.querySelector('.wrap-quantity') as HTMLInputElement;
-// let cartItemResult = document.querySelector('.result') as HTMLDivElement;
+if(window.location.href === 'http://localhost:8080/cart.html'){
+    (function(){
+        let cartItemImg = document.querySelector('.cartImg') as HTMLImageElement;
+        let cartItemName = document.querySelector('.discription') as HTMLDivElement;
+        let cartItemPrice = document.querySelector('.price') as HTMLDivElement;
+        let cartItemQuantity = document.querySelector('.wrap-quantity') as HTMLInputElement;
+        let cartItemResult = document.querySelector('.result') as HTMLDivElement;
+    
+        
+        let id = Number(localStorage.getItem('currentItemId'));
+        let currentQuantity = localStorage.getItem('currentQuantity');
+        database.forEach(elem => {
+            if(elem.id === id) {
+                cartItemImg.src = elem.photo[0];
+                cartItemName.textContent = elem.name;
+                cartItemPrice.textContent = elem.price;
+                cartItemQuantity.value = currentQuantity!;
 
-// // cartItemQuantity.value = '2';
-// let id = Number(localStorage.getItem('currentItemId'));
-// let currentQuantity = localStorage.getItem('currentQuantity');
-// database.forEach(elem => {
-//     if(elem.id === id) {
-//         cartItemImg.src = elem.photo[0];
-//         cartItemName.textContent = elem.name;
-//         cartItemPrice.textContent = elem.price;
-//         // cartItemQuantity.value = currentQuantity;
-//     }
-// })
-// console.log(localStorage.getItem('currentItemId'));
+                let priceNatural = parseInt(elem.price.replace('$', ''));
+                let str = String(priceNatural * Number(currentQuantity));
+                cartItemResult.textContent = '$' + str;
+            }
+        })
+    })();
+    
+    // console.log(localStorage.getItem('currentItemId'));
+}
--- online-store/src/assets/ts/item.ts
+++ online-store/src/assets/ts/item.ts
@@ -18,95 +18,97 @@ import database from './database'
     })
 })();
 
-// Get item data
-(function(){
-    const id = localStorage.getItem('currentItemId');
-    let num = Number(id);
-    // console.log(num);
+if(window.location.href === 'http://localhost:8080/item.html') {
+    // Get item data
+    (function(){
+        const id = localStorage.getItem('currentItemId');
+        let num = Number(id);
+        // console.log(num);
 
-    let itemImg = document.querySelector('.itemImg') as HTMLImageElement;
-    let itemPrice = document.querySelector('.cust') as HTMLParagraphElement;
-    let itemName = document.querySelector('.current-item') as HTMLSpanElement;
-    let itemKind = document.querySelector('.kinds') as HTMLSpanElement;
-    let discription = document.querySelector('.discription') as HTMLDivElement;
+        let itemImg = document.querySelector('.itemImg') as HTMLImageElement;
+        let itemPrice = document.querySelector('.cust') as HTMLParagraphElement;
+        let itemName = document.querySelector('.current-item') as HTMLSpanElement;
+        let itemKind = document.querySelector('.kinds') as HTMLSpanElement;
+        let discription = document.querySelector('.discription') as HTMLDivElement;
 
-    database.forEach(elem => {
-        if(elem.id === num) {
-            itemImg.src = elem.photo[0];
-            itemPrice.textContent = elem.price;
-            itemName.textContent = elem.name;
-            itemKind.textContent = elem.category;
-            discription.textContent = elem.description;
-        }
-    })
-})();
-
-// Choose the size
-(function(){
-    let parent = document.querySelector('.sizes') as HTMLDivElement;
-
-    parent.addEventListener('click', (e) => {
-        let target = e.target as HTMLDivElement;
-
-        let sizes = document.querySelectorAll('.size')
-        sizes.forEach(item => {
-            item.classList.remove('active-size');
-        })
-
-        target.classList.add('active-size');
-
-        // Push size to local storage
-        sizes.forEach(elem => {
-            if(elem.classList.contains('active-size')) {
-                localStorage.setItem('currentSize', `${elem.attributes[1].nodeValue}`)
+        database.forEach(elem => {
+            if(elem.id === num) {
+                itemImg.src = elem.photo[0];
+                itemPrice.textContent = elem.price;
+                itemName.textContent = elem.name;
+                itemKind.textContent = elem.category;
+                discription.textContent = elem.description;
             }
         })
+    })();
 
-        // console.log(localStorage.getItem('currentSize'));
-    })
-})();
+    // Choose the size
+    (function(){
+        let parent = document.querySelector('.sizes') as HTMLDivElement;
 
-// // Choose the color
-(function(){
-    let parent = document.querySelector('.colors') as HTMLDivElement;
+        parent.addEventListener('click', (e) => {
+            let target = e.target as HTMLDivElement;
 
-    parent.addEventListener('click', (e) => {
-        let target = e.target as HTMLDivElement;
+            let sizes = document.querySelectorAll('.size')
+            sizes.forEach(item => {
+                item.classList.remove('active-size');
+            })
 
-        let colors = document.querySelectorAll('.color');
-        colors.forEach(item => {
-            item.classList.remove('active-color');
+            target.classList.add('active-size');
+
+            // Push size to local storage
+            sizes.forEach(elem => {
+                if(elem.classList.contains('active-size')) {
+                    localStorage.setItem('currentSize', `${elem.attributes[1].nodeValue}`)
+                }
+            })
+
+            // console.log(localStorage.getItem('currentSize'));
         })
+    })();
 
-        target.classList.add('active-color');
+    // // Choose the color
+    (function(){
+        let parent = document.querySelector('.colors') as HTMLDivElement;
 
-        // Push size to local storage
-        colors.forEach(elem => {
-            if(elem.classList.contains('active-color')) {
-                localStorage.setItem('currentColor', `${elem.attributes[1].nodeValue}`)
-            }
+        parent.addEventListener('click', (e) => {
+            let target = e.target as HTMLDivElement;
+
+            let colors = document.querySelectorAll('.color');
+            colors.forEach(item => {
+                item.classList.remove('active-color');
+            })
+
+            target.classList.add('active-color');
+
+            // Push size to local storage
+            colors.forEach(elem => {
+                if(elem.classList.contains('active-color')) {
+                    localStorage.setItem('currentColor', `${elem.attributes[1].nodeValue}`)
+                }
+            })
+
+            // console.log(localStorage.getItem('currentColor'));
         })
-
-        // console.log(localStorage.getItem('currentColor'));
-    })
-})();
+    })();
 
 
-// // Push to lockal storage quantity goods
-(function(){
-    let input = document.querySelector('.quantity') as HTMLInputElement;
-    localStorage.setItem('currentQuantity', `${input.value}`);
-    // console.log(localStorage.getItem('currentQuantity'));
-    
-    input.addEventListener('change', () => {
-        
+    // // Push to lockal storage quantity goods
+    (function(){
+        let input = document.querySelector('.quantity') as HTMLInputElement;
         localStorage.setItem('currentQuantity', `${input.value}`);
-        
         // console.log(localStorage.getItem('currentQuantity'));
-    })
-})();
+        
+        input.addEventListener('change', () => {
+            
+            localStorage.setItem('currentQuantity', `${input.value}`);
+            
+            // console.log(localStorage.getItem('currentQuantity'));
+        })
+    })();
 
 
-// // add btn listener
-let btn = document.querySelector('.add-btn') as HTMLButtonElement;
-btn.addEventListener('click', () => {window.location.href = './cart.html'});
+    // // add btn listener
+    let btn = document.querySelector('.add-btn') as HTMLButtonElement;
+    btn.addEventListener('click', () => {window.location.href = './cart.html'});
+}
